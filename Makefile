bced838fe9a45a886719a309f96df90ba07da2ec
UnNomBidon <virgilebrian@gmail.com>
2022-12-27 17:59:41 +0100

    first commit

--- Makefile
+++ Makefile
@@ -7,5 +7,8 @@ main.o: main.cpp utils.o
 utils.o: utils.h utils.cpp
 	g++ -c -Wall --std=c++17 utils.cpp -o bin/utils.o
 
+Matrix.o: Matrix.h Matrix.cpp
+	g++ -c -Wall --std=c++17 Matrix.cpp -o bin/Matrix.o
+
 clean:
 	rm -rf bin/*.o bin/main *.o
--- main.cpp
+++ main.cpp
@@ -8,10 +8,18 @@
 
 #include <iostream>
 #include "utils.h"
+#include "Matrix.h"
 
 using namespace std;
 
 
 int main(int argc, char **argv){
+    Matrix data = Matrix(3, 3);
+    for(int i=0; i<data.size(); ++i)
+    {
+        data(i,i)=1;
+    }
+    data.print();
+
     return 0;
 };
