d953ad5a5949fbbddf65edecc2efbeb709be4e49
QAvare <quentin.avare@gmail.com>
2023-01-08 22:02:49 +0100

    added multiply matrix and scalar

--- Matrix.h
+++ Matrix.h
@@ -3,19 +3,23 @@
 
 class Matrix
 {
-    public:
-        Matrix();
-        Matrix(int m, int n);
-        int rows() const;
-        int cols() const;
-        double& operator() (int i, int j);
-        Matrix Invert();
-        int size() const;
-        void print();
+public:
+    Matrix();
+    Matrix(int m, int n);
+    
+    int rows() const;
+    int cols() const;
+    int size() const;
+    
+    Matrix Invert();
+    Matrix dot(Matrix m);
+    void print();
+    
+    Matrix operator*(double x);
+    double &operator()(int i, int j);
 
-
-    private:
-        int rows_;
-        int cols_;
-        double **data_;
+private:
+    int rows_;
+    int cols_;
+    double **data_;
 };
