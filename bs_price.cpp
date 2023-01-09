53d768d78314665576bf5794285c0bb6f700b31d
QAvare <quentin.avare@gmail.com>
2023-01-09 15:34:10 +0100

    refactor code

--- bs_price.cpp
+++ bs_price.cpp
@@ -1,22 +1,29 @@
 #include <cmath>
 #include <algorithm>
 
-double cdf(double x) {
-  return 0.5 * std::erfc(-x / std::sqrt(2));
+double cdf(double x)
+{
+    return 0.5 * std::erfc(-x / std::sqrt(2));
 }
 
-double blackScholes(char callPutFlag, double S, double K, double r, double q, double sigma, double T) {
-  double d1 = (std::log(S / K) + (r - q + 0.5 * sigma * sigma) * T) / (sigma * std::sqrt(T));
-  double d2 = d1 - sigma * std::sqrt(T);
-  double price;
+double blackScholes(char callPutFlag, double S, double K, double r, double q, double sigma, double T)
+{
+    double d1 = (std::log(S / K) + (r - q + 0.5 * sigma * sigma) * T) / (sigma * std::sqrt(T));
+    double d2 = d1 - sigma * std::sqrt(T);
+    double price;
 
-  if (callPutFlag == 'c') {
-    price = S * std::exp(-q * T) * cdf(d1) - K * std::exp(-r * T) * cdf(d2);
-  } else if (callPutFlag == 'p') {
-    price = K * std::exp(-r * T) * cdf(-d2) - S * std::exp(-q * T) * cdf(-d1);
-  } else {
-    throw "Invalid callPutFlag";
-  }
+    if (callPutFlag == 'c')
+    {
+        price = S * std::exp(-q * T) * cdf(d1) - K * std::exp(-r * T) * cdf(d2);
+    }
+    else if (callPutFlag == 'p')
+    {
+        price = K * std::exp(-r * T) * cdf(-d2) - S * std::exp(-q * T) * cdf(-d1);
+    }
+    else
+    {
+        throw "Invalid callPutFlag";
+    }
 
-  return price;
+    return price;
 }
